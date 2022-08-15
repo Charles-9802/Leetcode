06f736a30e8094a952054a4032974a275a5ac8bd
zhangxu <20120325@bjtu.edu.cn>
2022-08-15 20:55:56 +0800

    offferII064

--- offerII064.py
+++ offerII064.py
@@ -4,13 +4,33 @@ class MagicDictionary:
         """
         Initialize your data structure here.
         """
+        self.map =[]
 
-
-    def buildDict(self, dictionary: List[str]) -> None:
-
+    def buildDict(self, dictionary) -> None:
+        self.map.append(dictionary)
 
     def search(self, searchWord: str) -> bool:
+        def bijiao(ch, searchWord):
+            if len(ch) != len(searchWord):
+                return 0
+            else:
+                count = 0
+                for i in range(len(ch)):
+                    if ch[i] != searchWord[i]:
+                        count += 1
+                if count == 1:
+                    return 1
+                else:
+                    return 0
 
+        res = []
+        for ch in self.map[0]:
+            temp = bijiao(ch, searchWord)
+            res.append(temp)
+        if 1 in res:
+            return True
+        else:
+            return False
 
 
 # Your MagicDictionary object will be instantiated and called as such:
