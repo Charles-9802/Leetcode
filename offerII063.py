71e7687c8138511d823587dc152953b05a7d6293
zhangxu <20120325@bjtu.edu.cn>
2022-08-17 16:36:59 +0800

    offerII063

--- offerII063.py
+++ offerII063.py
@@ -1,2 +1,25 @@
+# hashset 遍历速度快
 class Solution:
-    def replaceWords(self, dictionary, sentence: str) -> str:
+    def replaceWords(self, dictionary, sentence: str):
+        res = []
+        sentence = sentence.split(" ")
+        hashset = set(dictionary)
+        for ch in sentence:
+            res.append(ch)
+            for i in range(len(ch)):
+                if ch[:i] in hashset:
+                    res.pop()
+                    res.append(ch[:i])
+                    break
+        return " ".join(res)
+
+
+def main():
+    dictionary = ["cat", "bat", "rat"]
+    sentence = "the cattle was rattled by the battery"
+    A = Solution()
+    print(A.replaceWords(dictionary, sentence))
+
+
+if __name__== "__main__":
+    main()
--- test.py
+++ test.py
@@ -1,12 +1,12 @@
-def check(arr):
-    dic = {}
-    for num in arr:
-        dic[num] = dic.get(num, 0) + 1
-    return max(dic, key=lambda x: dic[x])
-    # return max(dic, key=dic.get)
-
-
-print(check([1, 1, 1, 2, 2]))
+# def check(arr):
+#     dic = {}
+#     for num in arr:
+#         dic[num] = dic.get(num, 0) + 1
+#     return max(dic, key=lambda x: dic[x])
+#     # return max(dic, key=dic.get)
+#
+#
+# print(check([1, 1, 1, 2, 2]))
 
 # out = []
 # def swy(seq):
@@ -19,5 +19,9 @@ print(check([1, 1, 1, 2, 2]))
 #
 #
 # print(swy([1, [2, 3], [[4, 5], [6]]]))
+
+sentence = "the cattle was rattled by the battery"
+sentence = sentence.split(" ")
+print(sentence)
 
 
