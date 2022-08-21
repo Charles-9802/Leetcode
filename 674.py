ad5d0a67f70914933fc46e26c3fe69daa184ebe6
zhangxu <20120325@bjtu.edu.cn>
2022-08-21 20:43:21 +0800

    动态规划674

--- 674.py
+++ 674.py
@@ -1,15 +1,25 @@
+# class Solution:
+#     def findLengthOfLCIS(self, nums) -> int:
+#         res = 1
+#         count = 1
+#         for i in range(1, len(nums)):
+#             if nums[i] > nums[i - 1]:
+#                 count += 1
+#             else:
+#                 res = max(count, res)
+#                 count = 1
+#         res = max(count, res)
+#         return res
+
 class Solution:
     def findLengthOfLCIS(self, nums) -> int:
-        res = 1
-        count = 1
-        for i in range(1, len(nums)):
-            if nums[i] > nums[i - 1]:
-                count += 1
-            else:
-                res = max(count, res)
-                count = 1
-        res = max(count, res)
-        return res
+        result = 1
+        dp = [1] * len(nums)
+        for i in range(len(nums) - 1):
+            if nums[i + 1] > nums[i]:   # 连续记录
+                dp[i + 1] = dp[i] + 1
+            result = max(result, dp[i+1])
+        return result
 
 
 def main():
