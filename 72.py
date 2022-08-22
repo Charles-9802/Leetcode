01fcca52b64c41a641f1a8898466a80db5b426da
zhangxu <20120325@bjtu.edu.cn>
2022-08-22 11:15:31 +0800

    动态规划72编辑距离

--- 72.py
+++ 72.py
@@ -2,31 +2,21 @@
 
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        n = len(word1)
-        m = len(word2)
-
-        if n == 0 and m == 0:
-            return 0
-        if n == 0 and m != 0:
-            return m
-        if n != 0 and m == 0:
-            return n
-
-        Dp = [[0] * (m + 1) for _ in range(n + 1)]
+        dp = [[0] * (len(word2) + 1) for _ in range(len(word1) + 1)]
 
         # 初始化
-        for i in range(n + 1):
-            Dp[i][0] = i
-        for j in range(m + 1):
-            Dp[0][j] = j
+        for i in range(len(word1) + 1):
+            dp[i][0] = i
+        for j in range(len(word2) + 1):
+            dp[0][j] = j
 
-        for i in range(1, n + 1):
-            for j in range(1, m + 1):
+        for i in range(1, len(word1) + 1):
+            for j in range(1, len(word2) + 1):
                 if word1[i - 1] != word2[j - 1]:
-                    Dp[i][j] = min(Dp[i-1][j] + 1, Dp[i][j - 1] + 1, Dp[i - 1][j - 1] + 1)
+                    dp[i][j] = min(dp[i-1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + 1)
                 else:
-                    Dp[i][j] = min(Dp[i - 1][j] + 1, Dp[i][j - 1] + 1, Dp[i - 1][j - 1])
-        return Dp[-1][-1]
+                    dp[i][j] = dp[i-1][j-1]
+        return dp[-1][-1]
 
 
 def main():
